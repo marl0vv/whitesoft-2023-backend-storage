756b19ebaf87c08385ab0c129f1a54b112d07203
marl0vv <23glad02@mail.ru>
2023-10-24 14:37:01 +1000

    Update Main to run program

--- src/main/java/com/marl0vv/study/Main.java
+++ src/main/java/com/marl0vv/study/Main.java
@@ -2,6 +2,8 @@ package com.marl0vv.study;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Storage storage = new Storage("./src/main/resources/inputData.json");
+        Service service = new Service(storage);
+        service.run();
     }
 }
