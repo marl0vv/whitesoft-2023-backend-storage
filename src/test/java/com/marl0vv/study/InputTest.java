81cd15acce319f38346274f1a1df23970847e7ed
marl0vv <23glad02@mail.ru>
2023-11-12 22:04:19 +1000

    Added comments with Arrange, Act, Assert in tests

--- src/test/java/com/marl0vv/study/InputTest.java
+++ src/test/java/com/marl0vv/study/InputTest.java
@@ -9,22 +9,32 @@ import static org.junit.jupiter.api.Assertions.*;
 public class InputTest {
     @Test
     void testWrittenDataIsNotEmpty() {
+        //Arrange
         Map<Integer, Entry> storage = Input.readFromFile("./src/test/resources/inputData.json");
+
+        //Assert
         assertFalse(storage.isEmpty());
     }
 
     @Test
     public void testReadFromFileInvalidFile() {
+        //Arrange
         Map<Integer, Entry> storage = Input.readFromFile("nonExistentFile.json");
+
+        //Assert
         assertTrue(storage.isEmpty());
     }
 
     @Test
     public void testReadFromFileValidFile() {
+        //Arrange
         Map<Integer, Entry> storage = Input.readFromFile("./src/test/resources/inputData.json");
-
         Entry expectedEntry = new Entry(1, "The Shape of Punk To Come", "Hardcore punk", "https://en.wikipedia.org/wiki/The_Shape_of_Punk_to_Come");
+
+        //Act
         Entry actualEntry = storage.get(1);
+
+        //Assert
         assertEquals(expectedEntry, actualEntry);
     }
 }
--- src/test/java/com/marl0vv/study/ServiceTest.java
+++ src/test/java/com/marl0vv/study/ServiceTest.java
@@ -15,31 +15,44 @@ public class ServiceTest {
 
     @BeforeAll
     void setupStorage() {
+        //Arrange
         storage.initializeStorage("./src/test/resources/inputData.json");
     }
 
     @Test
     void testFindById() {
+        //Arrange
         int testId = 3;
+
+        //Act
         Entry actualEntry = storage.findByIdOrNull(testId);
 
+        //Assert
         assertNotNull(actualEntry);
         assertEquals("Southern Air", actualEntry.getName());
     }
 
     @Test
     void testFindByErrorId() {
+        //Arrange
         int testId = Integer.MIN_VALUE;
+
+        //Act
         Entry actualEntry = storage.findByIdOrNull(testId);
 
+        //Assert
         assertNull(actualEntry);
     }
 
     @Test
     void testFindByName() {
+        //Arrange
         String testName = "Shape";
+
+        //Act
         List<Entry> matchingEntries = storage.findByName(testName);
 
+        //Assert
         assertNotNull(matchingEntries);
         for (Entry entry : matchingEntries) {
             assertTrue(entry.getName().toLowerCase().contains(testName.toLowerCase()));
@@ -48,9 +61,13 @@ public class ServiceTest {
 
     @Test
     void testFindByErrorName() {
+        //Arrange
         String testName = "eofaRorRfz";
+
+        //Act
         List<Entry> matchingEntries = storage.findByName(testName);
 
+        //Assert
         assertNotNull(matchingEntries);
         assertTrue(matchingEntries.isEmpty());
     }
